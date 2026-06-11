d9b00d9be287f69ee44708530d84cd1c6784d0ad
bear <bear@crumpton.org>
2026-06-11 17:43:47 -0600

    chore: remove legacy host function mappings and bump version to 0.3.62

--- host/host.go
+++ host/host.go
@@ -44,49 +44,6 @@ func MakeHostObject(cfg config.Config, ctx context.Context, pluginName string) m
 	pid := fmt.Sprintf("%d", os.Getpid())
 
 	return map[string]interface{}{
-		// Legacy names will be deprecated in the future
-		"readFile":   func(path string) (string, error) { return fs.ReadFile(path, cfg, pluginName) },
-		"writeFile":  func(path string, content string) error { return fs.WriteFile(path, content, cfg, pluginName) },
-		"listFiles":  func(path string) ([]string, error) { return fs.ListFiles(path, cfg, pluginName) },
-		"deleteFile": func(path string) error { return fs.DeleteFile(path, cfg, pluginName) },
-		"renameFile": func(oldPath, newPath string) error {
-			return fs.RenameFile(oldPath, newPath, cfg, pluginName)
-		},
-		"runCommand": func(command string) (string, error) { return exec.RunCommand(ctx, cfg, pluginName, command) },
-		"httpGet": func(urlStr string, headers map[string]interface{}) (map[string]interface{}, error) {
-			return httpclient.Get(ctx, urlStr, headers, cfg, pluginName)
-		},
-		"httpPost": func(urlStr string, headers map[string]interface{}, body string) (map[string]interface{}, error) {
-			return httpclient.Post(ctx, urlStr, headers, body, cfg, pluginName)
-		},
-		"httpPut": func(urlStr string, headers map[string]interface{}, body string) (map[string]interface{}, error) {
-			return httpclient.Put(ctx, urlStr, headers, body, cfg, pluginName)
-		},
-		"httpDelete": func(urlStr string, headers map[string]interface{}) (map[string]interface{}, error) {
-			return httpclient.Delete(ctx, urlStr, headers, "", cfg, pluginName)
-		},
-		"getEnv": func(key string) (string, error) {
-			return env.GetEnv(key, cfg, pluginName)
-		},
-		"readStream": func(path string, options map[string]interface{}, callback func(chunk string) error) error {
-			return fs.ReadStream(path, cfg, pluginName, options, callback)
-		},
-		"writeStream": func(path string, options map[string]interface{}, callback func() (string, error)) error {
-			return fs.WriteStream(path, cfg, pluginName, options, callback)
-		},
-		"makeDir": func(path string) error {
-			return fs.Mkdir(path, cfg, pluginName)
-		},
-		"rmDir": func(path string) error {
-			return fs.RmDir(path, cfg, pluginName)
-		},
-		"isdir": func(path string) (bool, error) {
-			return fs.IsDir(path, cfg, pluginName)
-		},
-		"isexisted": func(path string) (bool, error) {
-			return fs.IsPathExisted(path, cfg, pluginName)
-		},
-
 		// Informational
 		"logger":      cfg.LogfForPlugin(pluginName),
 		"pluginConfig": pluginConfig,
